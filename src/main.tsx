e6fec4b1c0ac1d88f26445242cc741df0d858c94
Thành Trang <14819342+trangcongthanh@users.noreply.github.com>
2022-10-04 11:21:08 +0700

    fix: add minimal reset

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes/router'
+import './main.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
